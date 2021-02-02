9452a49f3f353c39bdfff8648d52d4d1e7fc7c80
iamrjrobin <iamrjrobin@hotmail.com>
2021-02-02 18:25:46 +0600

    static root added

--- house_points/settings.py
+++ house_points/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'basic_user/static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
